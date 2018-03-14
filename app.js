84c2810a06db26e515cce9db10bdab36938cdaf6
Jorge Valdeiglesias <12958852+argjv@users.noreply.github.com>
2018-03-13 20:33:22 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cluster = require('cluster');
+const Tokenizer = require('./lib/tokenizer');
+const Mapper = require('./lib/mapper');
+const Store = require('./lib/inputStore');
+const indexer = {
+  tokenizer: Tokenizer,
+  mapper: Mapper,
+  store: Store
+}
+init(process.argv[2], process.argv[3]);
+function init(type, numCPU)  {
+  if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    for (let i = 0; i < numCPU; i++)  {
+      cluster.fork();
+    }
+    cluster.on('death', function(worker) {
+      console.log(`Worker ${process.pid} has died`);
+    });
+  } else {
+    const service = new indexer[type]();
+    service.start();
+  }
+}
